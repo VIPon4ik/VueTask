8c48155e4560edf8cf69dce72657e50cffb9ca0a
VIPon4ik <umniyd@gmail.com>
2024-01-14 21:04:47 +0200

    Styled input component

--- frontend/src/components/ResumeForm.vue
+++ frontend/src/components/ResumeForm.vue
@@ -1,5 +1,6 @@
 <template>
   <form>
+    <h1>Create your Resume</h1>
     <my-input></my-input>
   </form>
 </template>
@@ -9,7 +10,17 @@ export default {
 }
 </script>
 <style scoped>
+  h1 {
+    font-size: 20px;
+    text-align: center;
+    margin-bottom: 20px;
+  }
+
   form {
     margin: 0 auto;
+    max-width: 280px;
+    border-radius: 4px;
+    background-color: white;
+    padding: 20px;
   }
 </style>
--- frontend/src/components/ui/MyInput.vue
+++ frontend/src/components/ui/MyInput.vue
@@ -1,5 +1,5 @@
 <template>
-  <input type="text">
+  <input type="text" placeholder="some text">
 </template>
 <script>
 export default {
@@ -7,5 +7,17 @@ export default {
 }
 </script>
 <style>
+  input {
+    width: 100%;
+    padding: 0px 16px;
+    height: 40px;
+    border: none;
+    background-color: rgb(228, 234, 247);
+    outline: none;
+    color: rgb(61,61,61);
+  }
   
+  input::placeholder {
+    color: rgb(122, 122, 122);
+  }
 </style>
